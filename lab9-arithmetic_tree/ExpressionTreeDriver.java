2ae70d371539720f537e241228e336804f0f411a
stonesea9 <stonesea9@gmail.com>
2026-03-27 11:45:18 -0700

    Update ExpressionTreeDriver.java

--- lab9-arithmetic_tree/ExpressionTreeDriver.java
+++ lab9-arithmetic_tree/ExpressionTreeDriver.java
@@ -1,1 +1,45 @@
 
+public class ExpressionTreeDriver {
+    public static void main(String[] args) {
+        // Build the tree for: ((3 + 7) * (9 - 4))
+
+        BinaryTreeNode root = new BinaryTreeNode("*");
+
+        BinaryTreeNode nodePlus = new BinaryTreeNode("+");
+        BinaryTreeNode nodeMinus = new BinaryTreeNode("-");
+
+        // Connect root to children
+        root.left = nodePlus;
+        root.right = nodeMinus;
+        nodePlus.parent = root;
+        nodeMinus.parent = root;
+
+        // Create leaves
+        BinaryTreeNode node3 = new BinaryTreeNode("3");
+        BinaryTreeNode node7 = new BinaryTreeNode("7");
+        BinaryTreeNode node9 = new BinaryTreeNode("9");
+        BinaryTreeNode node4 = new BinaryTreeNode("4");
+
+        // Connect leaves to nodePlus
+        nodePlus.left = node3;
+        nodePlus.right = node7;
+        node3.parent = nodePlus;
+        node7.parent = nodePlus;
+
+        // Connect leaves to nodeMinus
+        nodeMinus.left = node9;
+        nodeMinus.right = node4;
+        node9.parent = nodeMinus;
+        node4.parent = nodeMinus;
+
+        // Perform traversals
+        System.out.println("--- Preorder Traversal (Prefix) ---");
+        root.traversePreorder();
+
+        System.out.println("\n\n--- Inorder Traversal (Infix) ---");
+        root.traverseInorder();
+
+        System.out.println("\n\n--- Postorder Traversal (Postfix) ---");
+        root.traversePostorder();
+    }
+}
