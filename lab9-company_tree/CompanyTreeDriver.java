50ed4ba319ad5290be00a1ba026d151468c04d6f
stonesea9 <stonesea9@gmail.com>
2026-03-27 11:27:59 -0700

    Update CompanyTreeDriver.java

--- lab9-company_tree/CompanyTreeDriver.java
+++ lab9-company_tree/CompanyTreeDriver.java
@@ -1,1 +1,38 @@
 
+public class CompanyTreeDriver {
+    public static void main(String[] args) {
+        // 1. Build the Tree
+        GeneralTreeNode root = new GeneralTreeNode("CEO");
+
+        // Create department heads
+        GeneralTreeNode vpSales = new GeneralTreeNode("VP of Sales");
+        GeneralTreeNode vpEngineering = new GeneralTreeNode("VP of Engineering");
+        root.addChild(vpSales);
+        root.addChild(vpEngineering);
+
+        // Add children for VP of Sales
+        GeneralTreeNode salesNA = new GeneralTreeNode("Sales Manager (NA)");
+        GeneralTreeNode salesEU = new GeneralTreeNode("Sales Manager (EU)");
+        vpSales.addChild(salesNA);
+        vpSales.addChild(salesEU);
+
+        // Add children for VP of Engineering
+        GeneralTreeNode devLead = new GeneralTreeNode("Dev Team Lead");
+        GeneralTreeNode qaLead = new GeneralTreeNode("QA Team Lead");
+        vpEngineering.addChild(devLead);
+        vpEngineering.addChild(qaLead);
+
+        // Add developers under Dev Team Lead
+        GeneralTreeNode dev1 = new GeneralTreeNode("Developer 1");
+        GeneralTreeNode dev2 = new GeneralTreeNode("Developer 2");
+        devLead.addChild(dev1);
+        devLead.addChild(dev2);
+
+        // 2. Perform Traversals
+        System.out.println("--- Preorder Traversal (Company Hierarchy) ---");
+        root.traversePreorder();
+
+        System.out.println("\n--- Postorder Traversal (Staff Roll Call) ---");
+        root.traversePostorder();
+    }
+}
