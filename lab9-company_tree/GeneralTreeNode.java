8106970a44b54d5a1cb46f81bc3aed7127deaa60
stonesea9 <stonesea9@gmail.com>
2026-03-27 11:28:54 -0700

    Update GeneralTreeNode.java

--- lab9-company_tree/GeneralTreeNode.java
+++ lab9-company_tree/GeneralTreeNode.java
@@ -1,1 +1,47 @@
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneralTreeNode {
+    String name; // Employee name or department title
+    GeneralTreeNode parent;
+    List<GeneralTreeNode> children;
+
+    public GeneralTreeNode(String name) {
+        this.name = name;
+        this.parent = null;
+        this.children = new ArrayList<>();
+    }
+
+    // Method to add a child to this node
+    public void addChild(GeneralTreeNode child) {
+        child.parent = this;
+        this.children.add(child);
+    }
+
+    /**
+     * Performs a preorder traversal starting from this node.
+     * Prints the name of each node visited.
+     * (Visit Parent, then visit children)
+     */
+    public void traversePreorder() {
+        System.out.println(this.name);
+
+        for (GeneralTreeNode child : children) {
+            child.traversePreorder();
+        }
+    }
+
+    /**
+     * Performs a postorder traversal starting from this node.
+     * Prints the name of each node visited.
+     * (Visit children, then visit Parent)
+     */
+    public void traversePostorder() {
+        for (GeneralTreeNode child : children) {
+            child.traversePostorder();
+        }
+
+        System.out.println(this.name);
+    }
+}
