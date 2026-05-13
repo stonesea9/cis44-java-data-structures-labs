d501c140c2bff990dcdf97a250432aff3954a5a6
stonesea9 <stonesea9@gmail.com>
2026-05-12 23:02:22 -0700

    Update Main.java

--- lab14-Maps_Hash_Tables/Main.java
+++ lab14-Maps_Hash_Tables/Main.java
@@ -31,14 +31,34 @@ public class Main {
     }
 
     public static void testSeparateChainingMap() {
-        SeparateChainingMap<Integer, String> map = new SeparateChainingMap<>();
+        SeparateChainingMap<String, String> map = new SeparateChainingMap<>();
 
-        System.out.println("Testing SeparateChainingMap:");
-        System.out.println("put(5, A): " + map.put(5, "A"));
-        System.out.println("put(7, B): " + map.put(7, "B"));
-        System.out.println("put(2, C): " + map.put(2, "C"));
-        System.out.println("get(7): " + map.get(7));
-        System.out.println("get(99): " + map.get(99));
-        System.out.println("remove(5): " + map.remove(5));
+        System.out.println("Testing SeparateChainingMap with collision handling:");
+        System.out.println();
+
+        System.out.println("put(AA, Patient A): " + map.put("AA", "Patient A"));
+        System.out.println("put(AL, Patient B): " + map.put("AL", "Patient B"));
+        System.out.println("put(AW, Patient C): " + map.put("AW", "Patient C"));
+
+        System.out.println();
+
+        System.out.println("get(AA): " + map.get("AA"));
+        System.out.println("get(AL): " + map.get("AL"));
+        System.out.println("get(AW): " + map.get("AW"));
+
+        System.out.println();
+
+        System.out.println("put(AL, Updated Patient B): " + map.put("AL", "Updated Patient B"));
+        System.out.println("get(AL): " + map.get("AL"));
+
+        System.out.println();
+
+        System.out.println("remove(AA): " + map.remove("AA"));
+        System.out.println("get(AA): " + map.get("AA"));
+        System.out.println("size(): " + map.size());
+
+        System.out.println();
+
+        System.out.println("Collision test completed.");
     }
 }
