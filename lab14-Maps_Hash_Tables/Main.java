1668d8cfac84d63c7b6d645d71ce33045ca24a03
stonesea9 <stonesea9@gmail.com>
2026-05-12 22:58:30 -0700

    Update Main.java

--- lab14-Maps_Hash_Tables/Main.java
+++ lab14-Maps_Hash_Tables/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+/*public class Main {
     public static void main(String[] args) {
         UnsortedListMap<Integer, String> map = new UnsortedListMap<>();
 
@@ -7,6 +7,38 @@ public class Main {
         System.out.println("put(2, C): " + map.put(2, "C"));
         System.out.println("put(2, E): " + map.put(2, "E"));
         System.out.println("get(7): " + map.get(7));
+        System.out.println("remove(5): " + map.remove(5));
+    }
+}
+*/
+public class Main {
+    public static void main(String[] args) {
+        testUnsortedListMap();
+        System.out.println();
+        testSeparateChainingMap();
+    }
+
+    public static void testUnsortedListMap() {
+        UnsortedListMap<Integer, String> map = new UnsortedListMap<>();
+
+        System.out.println("Testing UnsortedListMap:");
+        System.out.println("put(5, A): " + map.put(5, "A"));
+        System.out.println("put(7, B): " + map.put(7, "B"));
+        System.out.println("put(2, C): " + map.put(2, "C"));
+        System.out.println("put(2, E): " + map.put(2, "E"));
+        System.out.println("get(7): " + map.get(7));
+        System.out.println("remove(5): " + map.remove(5));
+    }
+
+    public static void testSeparateChainingMap() {
+        SeparateChainingMap<Integer, String> map = new SeparateChainingMap<>();
+
+        System.out.println("Testing SeparateChainingMap:");
+        System.out.println("put(5, A): " + map.put(5, "A"));
+        System.out.println("put(7, B): " + map.put(7, "B"));
+        System.out.println("put(2, C): " + map.put(2, "C"));
+        System.out.println("get(7): " + map.get(7));
+        System.out.println("get(99): " + map.get(99));
         System.out.println("remove(5): " + map.remove(5));
     }
 }
