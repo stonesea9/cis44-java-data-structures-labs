178328e18e619217d69021c37629f57da92c5b4f
stonesea9 <stonesea9@gmail.com>
2026-05-12 22:47:58 -0700

    Create Main.java

---
+++ lab14-Maps_Hash_Tables/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        UnsortedListMap<Integer, String> map = new UnsortedListMap<>();
+
+        System.out.println("put(5, A): " + map.put(5, "A"));
+        System.out.println("put(7, B): " + map.put(7, "B"));
+        System.out.println("put(2, C): " + map.put(2, "C"));
+        System.out.println("put(2, E): " + map.put(2, "E"));
+        System.out.println("get(7): " + map.get(7));
+        System.out.println("remove(5): " + map.remove(5));
+    }
+}
